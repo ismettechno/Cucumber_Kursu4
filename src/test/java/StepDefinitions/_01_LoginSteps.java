fe3fcb06dbbd1a0306d4fbccf0e9f754cf877887
TechnoStudy <ismettechno@gmail.com>
2022-11-22 09:24:46 +0000

    Framework hazırlık 1

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,19 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _01_LoginSteps {
+    @Given("Naviagate to Campus")
+    public void naviagateToCampus() {
+    }
+
+    @When("Enter username and password and click Login button")
+    public void enterUsernameAndPasswordAndClickLoginButton() {
+    }
+
+    @Then("User should login successfuly")
+    public void userShouldLoginSuccessfuly() {
+    }
+}
